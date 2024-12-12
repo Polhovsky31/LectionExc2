984cde52f5fe03e834507c75835708e7f400e889
Polhovsky31 <71878410+Polhovsky31@users.noreply.github.com>
2024-12-12 15:00:59 +0300

    Update libmysyslog.c

--- libmysyslog/libmysyslog.c
+++ libmysyslog/libmysyslog.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#include <limits.h>
 
 static const char* log_levels[] = {"DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"};
 
@@ -22,8 +24,13 @@ int log_to_text(const char* msg, int level, const char* path) {
     FILE* file = fopen(path, "a");
     if (!file) return -1;
 
+    char proc_path[PATH_MAX];
+    if (readlink("/proc/self/exe", proc_path, sizeof(proc_path)) == -1) {
+        strcpy(proc_path, "unknown");
+    }
+
     time_t now = time(NULL);
-    fprintf(file, "%ld %s %s\n", now, log_levels[level], msg);
+    fprintf(file, "%ld %s [%s] (PID: %d) %s\n", now, log_levels[level], proc_path, getpid(), msg);
     fclose(file);
     return 0;
 }
@@ -32,8 +39,14 @@ int log_to_json(const char* msg, int level, const char* path) {
     FILE* file = fopen(path, "a");
     if (!file) return -1;
 
+    char proc_path[PATH_MAX];
+    if (readlink("/proc/self/exe", proc_path, sizeof(proc_path)) == -1) {
+        strcpy(proc_path, "unknown");
+    }
+
     time_t now = time(NULL);
-    fprintf(file, "{\"timestamp\":%ld,\"log_level\":\"%s\",\"message\":\"%s\"}\n", now, log_levels[level], msg);
+    fprintf(file, "{\"timestamp\":%ld,\"log_level\":\"%s\",\"process_path\":\"%s\",\"pid\":%d,\"message\":\"%s\"}\n",
+            now, log_levels[level], proc_path, getpid(), msg);
     fclose(file);
     return 0;
 }
